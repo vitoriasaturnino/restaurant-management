45efdb284c10e7765020966ce7b6bd867e91d3fe
vitoriasaturnino <vim.saturnino@gmail.com>
2023-08-08 20:00:08 -0300

    feat: add component to login page and integrate with mock data to make login functional

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,21 +1,18 @@
 <template>
   <img class="restaurant-logo" src="../assets/restaurant-logo.png"/>
-  <h1>Sing Up</h1>
+  <h1>LogIn</h1>
 
-  <div class="register-form">
-    <label for="email">Name</label>
-    <input type="text" v-model="name" placeholder="Enter your name">
-
-    <label for="email">E-mail</label>
+  <div class="login-form">
+    <label>E-mail</label>
     <input type="email" v-model="email" placeholder="Enter your e-mail">
 
-    <label for="email">Password</label>
-    <input type="password" v-model="password" placeholder="Enter you password">
+    <label>Password</label>
+    <input type="password" v-model="password" placeholder="Enter your password">
 
-    <button v-on:click="singUp" >Sing Up</button>
+    <button v-on:click="login" >Login</button>
 
     <p>
-      <router-link to="/login" >Login</router-link>
+      <router-link to="/sing-up" >Sing Up</router-link>
     </p>
   </div>
 </template>
@@ -24,27 +21,24 @@
   import axios from 'axios';
 
   export default ({
-    name: 'SingUp',
+    name: "LoginPage",
     data() {
       return {
-        name: '',
-        email: '',
-        password: ''
+        email: "",
+        password: ""
       }
     },
 
-    methods:{
-      async singUp() {
-        let result = await axios.post("http://localhost:3000/users", {
-          name: this.name,
-          email: this.email,
-          password: this.password
-        });
+    methods: {
+      async login() {
+        let result = await axios.get(
+          `http://localhost:3000/users?${this.email}=email$password=${this.password}`
+        );
 
         console.warn(result);
 
-        if (result.status == 201) {
-          localStorage.setItem("user-info", JSON.stringify(result.data));
+        if (result.status == 200 && result.data.length > 0) {
+          localStorage.setItem("user-info", JSON.stringify(result.data[0]));
           this.$router.push({ name: "Home"});
         }
       }
@@ -70,7 +64,7 @@
     height: 230px;
   }
 
-  .register-form label {
+  .login-form label {
     display: block;
     margin-bottom: 10px;
     margin-right: auto;
@@ -79,7 +73,7 @@
     text-align: left;
   }
 
-  .register-form input{
+  .login-form input{
     width: 300px;
     height: 40px;
     padding-left: 20px;
@@ -90,7 +84,7 @@
     border: 1px solid #494848;
   }
 
-  .register-form button {
+  .login-form button {
     width: 300px;
     height: 40px;
     background-color: #494848;
--- src/routers.js
+++ src/routers.js
@@ -1,5 +1,6 @@
 import HomePage from './components/HomePage.vue';
 import SingUp from './components/SingUp.vue';
+import LoginPage from './components/LoginPage.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
@@ -12,6 +13,11 @@ const routes = [
     name:'SingUp',
     component: SingUp,
     path:'/sing-up'
+  },
+  {
+    name:'LoginPage',
+    component: LoginPage,
+    path:'/login'
   }
 ];
 
