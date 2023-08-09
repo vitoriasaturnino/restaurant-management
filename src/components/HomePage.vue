3a44bedb472c5591b66107fb953df2e691e740b1
vitoriasaturnino <vim.saturnino@gmail.com>
2023-08-09 08:29:42 -0300

    feat: add list restaurants in home page

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,9 +1,26 @@
 <template>
   <HeaderBar />
   <h1>Hello {{ name }}, Welcome on Home Page!</h1>
+
+  <table border="1">
+    <tr>
+      <td>Id</td>
+      <td>Name</td>
+      <td>Contact</td>
+      <td>Address</td>
+    </tr>
+    <tr v-for="item in restaurant" :key="item.id">
+      <td> {{ item.id }}</td>
+      <td> {{ item.name }}</td>
+      <td> {{ item.contact }}</td>
+      <td> {{ item.address }}</td>
+    </tr>
+  </table>
 </template>
 
 <script>
+  import axios from "axios";
+
   import HeaderBar from "./HeaderBar.vue";
 
   export default ({
@@ -15,17 +32,30 @@
 
     data() {
       return {
-        name: ''
+        name: '',
+        restaurant: []
       }
     },
 
-    mounted() {
+    async mounted() {
       let user = localStorage.getItem("user-info");
       this.name = JSON.parse(user).name;
 
       if (!user) {
         this.$router.push({ name: 'SingUp' });
       }
+
+      let result = await axios.get('http://localhost:3000/restaurants');
+      console.warn(result);
+
+      this.restaurant = result.data;
     }
   });
 </script>
+
+<style>
+  td {
+    width: 160px;
+    height: 40px;
+  }
+</style>
