4c0669963e2cd0556b1117dc62dc29bd114f33c8
vitoriasaturnino <vim.saturnino@gmail.com>
2023-08-09 14:58:49 -0300

    feat: add form to save new restaurant

--- src/App.vue
+++ src/App.vue
@@ -22,4 +22,35 @@
     margin: 0;
     padding: 0;
   };
+
+  .add-new-restaurant-form label {
+    display: block;
+    margin-bottom: 10px;
+    margin-right: auto;
+    margin-left: auto;
+    width: 500px;
+    text-align: left;
+  }
+
+  .add-new-restaurant-form input{
+    width: 500px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-right: auto;
+    margin-left: auto;
+    border: 1px solid #494848;
+  }
+
+  .add-new-restaurant-form button {
+    width: 500px;
+    height: 40px;
+    background-color: #494848;
+    color: #ffffff;
+    border: none;
+    margin-right: auto;
+    margin-left: auto;
+    display: block;
+  }
 </style>
--- src/components/AddNewRestaurant.vue
+++ src/components/AddNewRestaurant.vue
@@ -1,9 +1,24 @@
 <template>
   <HeaderBar />
   <h1>Hello {{ name }}, Welcome on Add New Restaurant Page!</h1>
+
+  <form class="add-new-restaurant-form">
+    <label>Name</label>
+    <input type="text" v-model="restaurant.name" placeholder="Enter Restaurant Name" />
+
+    <label>Contact</label>
+    <input type="text" v-model="restaurant.contact" placeholder="Enter Restaurant Contact" />
+
+    <label>Address</label>
+    <input type="text" v-model="restaurant.address" placeholder="Enter Restaurant Address" />
+
+    <button v-on:click="addNewRestaurant" >Save</button>
+  </form>
 </template>
 
 <script>
+  import axios from 'axios';
+
   import HeaderBar from "./HeaderBar.vue";
 
   export default ({
@@ -15,7 +30,25 @@
 
     data() {
       return {
-        name: ''
+        name: '',
+        restaurant: {
+          name: '',
+          contact: '',
+          address: ''
+        }
+      }
+    },
+
+    methods: {
+      async addNewRestaurant() {
+        console.warn(this.restaurant);
+        const result = await axios.post("http://localhost:3000/restaurants", {
+          name: this.restaurant.name,
+          contact: this.restaurant.contact,
+          address: this.restaurant.address
+        });
+
+        console.warn("result", result);
       }
     },
 
